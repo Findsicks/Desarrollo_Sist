a15a329fabb4dc80a412d76c249447cb8618d9f7
Findsicks <101424357+Findsicks@users.noreply.github.com>
2024-04-03 12:40:24 -0300

    Trabajo Práctico I/O  -  File
    falta terminar actividad E y F de la 1

---
+++ workspace-spring-tool-suite-4-4.17.2.RELEASE/Flujo_de_Datos/src/main/java/Flujo_De_Datos/Clase_Sueldo_Bruto.java
@@ -0,0 +1,11 @@
+package Flujo_De_Datos;
+
+public class Clase_Sueldo_Bruto {
+	public static void main(String[] args) {
+		double valorHoraTrabajada = 10.0;
+		int HorasTrabajadas = 40;
+		double SueldoBruto = valorHoraTrabajada * HorasTrabajadas;
+		
+		System.out.println("El sueldo Bruto es: $" + SueldoBruto);
+	} 
+}
---
+++ workspace-spring-tool-suite-4-4.17.2.RELEASE/Flujo_de_Datos/src/main/java/Flujo_De_Datos/Perimetro.java
@@ -0,0 +1,10 @@
+package Flujo_De_Datos;
+
+public class Perimetro {
+	public static void main(String[] args) {
+		double SuperficieCuadrado = 16.0;
+		double lado = Math.sqrt(SuperficieCuadrado);
+		double perimetro = 4 * lado;
+		System.out.println("El perimetro del cuadrado es de " + perimetro + " Metros");
+	}
+}
