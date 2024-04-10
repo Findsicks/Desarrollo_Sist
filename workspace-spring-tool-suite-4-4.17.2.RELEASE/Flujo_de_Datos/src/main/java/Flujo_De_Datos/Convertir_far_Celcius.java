b2be4745f98349457f7c3e6e5680f412f9836eff
Findsicks <101424357+Findsicks@users.noreply.github.com>
2024-04-10 10:34:04 -0300

    Update Convertir_far_Celcius.java
    Se a actualizado el programa para que el usuario pueda poner los datos que el quiera sin utilizar scanner

--- workspace-spring-tool-suite-4-4.17.2.RELEASE/Flujo_de_Datos/src/main/java/Flujo_De_Datos/Convertir_far_Celcius.java
+++ workspace-spring-tool-suite-4-4.17.2.RELEASE/Flujo_de_Datos/src/main/java/Flujo_De_Datos/Convertir_far_Celcius.java
@@ -1,9 +1,34 @@
 package Flujo_De_Datos;
 
+import java.io.IOException;
+
 public class Convertir_far_Celcius {
-	public static void main(String[] args){
-		double valorFarh = 36.0;
-		double valorCelcius = (valorFarh - 32.0) *  0.55;
-		System.out.println("El valor de Celcius es " + valorCelcius); 
-	}
+    
+    public static void main(String[] args) {
+        Convertir_far_Celcius programa = new Convertir_far_Celcius();
+        double valorFarh = programa.obtenerValorFahrenheit("Ingrese el valor en grados Fahrenheit: ");
+        double valorCelcius = (valorFarh - 32.0) * 0.55;
+        System.out.println("El valor en Celsius es " + valorCelcius);
+    }
+
+    public double obtenerValorFahrenheit(String mensaje) {
+        System.out.print(mensaje);
+        double valor = Double.parseDouble(entradaDeDatos());
+        return valor;
+    }
+
+    public String entradaDeDatos() {
+        String cadena = "";
+        try {
+            int Byte = -1;
+            while ((Byte = System.in.read()) != '\n') {
+                if (Byte != 13)
+                    cadena += (char) Byte;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return cadena.trim(); 
 }
