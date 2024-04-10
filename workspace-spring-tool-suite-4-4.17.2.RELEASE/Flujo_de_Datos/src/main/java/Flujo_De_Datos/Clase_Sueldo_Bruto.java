e96a226440584835fb0bbfa6ac3f22146f8b68c6
Findsicks <101424357+Findsicks@users.noreply.github.com>
2024-04-10 10:33:11 -0300

    Update Clase_Sueldo_Bruto.java
    Se a actualizado el programa para que el usuario pueda poner los datos que el quiera sin utilizar scanner

--- workspace-spring-tool-suite-4-4.17.2.RELEASE/Flujo_de_Datos/src/main/java/Flujo_De_Datos/Clase_Sueldo_Bruto.java
+++ workspace-spring-tool-suite-4-4.17.2.RELEASE/Flujo_de_Datos/src/main/java/Flujo_De_Datos/Clase_Sueldo_Bruto.java
@@ -1,11 +1,42 @@
 package Flujo_De_Datos;
 
+import java.io.IOException;
+
 public class Clase_Sueldo_Bruto {
-	public static void main(String[] args) {
-		double valorHoraTrabajada = 10.0;
-		int HorasTrabajadas = 40;
-		double SueldoBruto = valorHoraTrabajada * HorasTrabajadas;
-		
-		System.out.println("El sueldo Bruto es: $" + SueldoBruto);
-	} 
+    
+    public static void main(String[] args) {
+        Clase_Sueldo_Bruto programa = new Clase_Sueldo_Bruto();
+        double valorHoraTrabajada = programa.obtenerValorHora("Ingrese el valor por hora trabajada: ");
+        int horasTrabajadas = programa.obtenerHoras("Ingrese la cantidad de horas trabajadas: ");
+        double sueldoBruto = valorHoraTrabajada * horasTrabajadas;
+        
+        System.out.println("El sueldo bruto es: $" + sueldoBruto);
+    }
+
+    public double obtenerValorHora(String mensaje) {
+        System.out.print(mensaje);
+        double valor = Double.parseDouble(entradaDeDatos());
+        return valor;
+    }
+
+    public int obtenerHoras(String mensaje) {
+        System.out.print(mensaje);
+        int horas = Integer.parseInt(entradaDeDatos());
+        return horas;
+    }
+
+    public String entradaDeDatos() {
+        String cadena = "";
+        try {
+            int Byte = -1;
+            while ((Byte = System.in.read()) != '\n') {
+                if (Byte != 13)
+                    cadena += (char) Byte;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return cadena.trim(); 
 }
