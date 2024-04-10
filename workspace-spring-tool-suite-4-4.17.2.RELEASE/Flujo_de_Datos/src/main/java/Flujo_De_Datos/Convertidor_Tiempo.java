000cd46c2ff074df1c5b54c0bed393f07febe167
Findsicks <101424357+Findsicks@users.noreply.github.com>
2024-04-10 10:58:22 -0300

    Update Convertidor_Tiempo.java
    Se a actualizado el programa para que el usuario pueda poner los datos que el quiera sin utilizar scanner

--- workspace-spring-tool-suite-4-4.17.2.RELEASE/Flujo_de_Datos/src/main/java/Flujo_De_Datos/Convertidor_Tiempo.java
+++ workspace-spring-tool-suite-4-4.17.2.RELEASE/Flujo_de_Datos/src/main/java/Flujo_De_Datos/Convertidor_Tiempo.java
@@ -1,9 +1,44 @@
 package Flujo_De_Datos;
 
-public class Convertidor_Tiempo {
-	public static void main(String[] args) {
-		int tiempo_Total_SEG = 3600;
-		
-		
-	}
+import java.io.IOException;
+
+public class Tiempo {
+    
+    public static void main(String[] args) {
+        Tiempo programa = new Tiempo();
+        int segundos = programa.obtenerTiempo("Ingrese el tiempo en segundos: ");
+        
+        int dias = segundos / (24 * 3600);
+        segundos = segundos % (24 * 3600);
+        
+        int horas = segundos / 3600;
+        segundos %= 3600;
+        
+        int minutos = segundos / 60;
+        segundos %= 60;
+        
+        System.out.println("El tiempo es de " + dias + " días, " + horas + " horas, " + minutos + " minutos y " + segundos + " segundos.");
+    }
+
+    public int obtenerTiempo(String mensaje) {
+        System.out.print(mensaje);
+        int tiempo = Integer.parseInt(entradaDeDatos());
+        return tiempo;
+    }
+
+    public String entradaDeDatos() {
+        String cadena = "";
+        try {
+            int Byte = -1;
+            while ((Byte = System.in.read()) != '\n') {
+                if (Byte != 13)
+                    cadena += (char) Byte;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return cadena.trim();
+    }
 }
