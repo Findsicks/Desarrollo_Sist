ea542b8eef8aace335b3e1a592494dfa58f5626e
Findsicks <101424357+Findsicks@users.noreply.github.com>
2024-04-10 10:34:54 -0300

    Update Perimetro.java
    Se a actualizado el programa para que el usuario pueda poner los datos que el quiera sin utilizar scanner

--- workspace-spring-tool-suite-4-4.17.2.RELEASE/Flujo_de_Datos/src/main/java/Flujo_De_Datos/Perimetro.java
+++ workspace-spring-tool-suite-4-4.17.2.RELEASE/Flujo_de_Datos/src/main/java/Flujo_De_Datos/Perimetro.java
@@ -1,10 +1,37 @@
 package Flujo_De_Datos;
 
+import java.io.IOException;
+
 public class Perimetro {
-	public static void main(String[] args) {
-		double SuperficieCuadrado = 16.0;
-		double lado = Math.sqrt(SuperficieCuadrado);
-		double perimetro = 4 * lado;
-		System.out.println("El perimetro del cuadrado es de " + perimetro + " Metros");
-	}
+    
+    public static void main(String[] args) {
+        Perimetro programa = new Perimetro();
+        double superficieCuadrado = programa.obtenerSuperficieCuadrado("Ingrese el valor de la superficie del cuadrado: ");
+        double lado = Math.sqrt(superficieCuadrado);
+        double perimetro = 4 * lado;
+        System.out.println("El perímetro del cuadrado es de " + perimetro + " metros.");
+    }
+
+    public double obtenerSuperficieCuadrado(String mensaje) {
+        System.out.print(mensaje);
+        double superficie = Double.parseDouble(entradaDeDatos());
+        return superficie;
+    }
+
+    public String entradaDeDatos() {
+        String cadena = "";
+        try {
+            int Byte = -1;
+            while ((Byte = System.in.read()) != '\n') {
+                if (Byte != 13)
+                    cadena += (char) Byte;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return cadena.trim(); 
+    }
 }
+
